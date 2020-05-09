4dabb5436a491dcb39388cb8eb7e2b2976b0d718
NetsvetayDO <nedeol@mail.ru>
2020-05-09 12:34:08 +0300

    code: исправление ошибок 09.05

--- 12/svg.cpp
+++ 12/svg.cpp
@@ -29,27 +29,30 @@ void max_width(const vector<size_t>& bins,double& maximum)
 
 void svg_rect(double x, double y, double width, double height,string stroke = "black",string fill = "black")
 {
-cout << "<rect x='"<<x<< "' y='" <<y<<"' width='" <<width <<"' height='" <<height <<"' stroke='"<<stroke<<"' fill='"<<fill<<"'/>";
+    cout << "<rect x='"<<x<< "' y='" <<y<<"' width='" <<width <<"' height='" <<height <<"' stroke='"<<stroke<<"' fill='"<<fill<<"'/>";
 }
 
 void
-svg_text(double left, double baseline, string text) {
-cout << "<text x='" << left << "' y='" << baseline <<"'>" <<text <<"</text>";
+svg_text(double left, double baseline, string text)
+{
+    cout << "<text x='" << left << "' y='" << baseline <<"'>" <<text <<"</text>";
 }
 
 void
-svg_begin(double width, double height) {
-cout << "<?xml version='1.0' encoding='UTF-8'?>\n";
-cout << "<svg ";
-cout << "width='" << width << "' ";
-cout << "height='" << height << "' ";
-cout << "viewBox='0 0 " << width << " " << height << "' ";
-cout << "xmlns='http://www.w3.org/2000/svg'>\n";
+svg_begin(double width, double height)
+{
+    cout << "<?xml version='1.0' encoding='UTF-8'?>\n";
+    cout << "<svg ";
+    cout << "width='" << width << "' ";
+    cout << "height='" << height << "' ";
+    cout << "viewBox='0 0 " << width << " " << height << "' ";
+    cout << "xmlns='http://www.w3.org/2000/svg'>\n";
 }
 
 void
-svg_end() {
-cout << "</svg>\n";
+svg_end()
+{
+    cout << "</svg>\n";
 }
 
 void show_histogram_svg(const vector<size_t>& bins,double min,double max,size_t bin_count)
@@ -62,6 +65,7 @@ void show_histogram_svg(const vector<size_t>& bins,double min,double max,size_t 
     const auto TEXT_WIDTH = 50;
     const auto BIN_HEIGHT = 30;
     const auto BLOCK_WIDTH = 10;
+    const auto GRAPH_WIDTH = IMAGE_WIDTH-TEXT_WIDTH;
     double top = 0;
     double maximum=bins[0];
     max_width(bins,maximum);
@@ -76,30 +80,81 @@ void show_histogram_svg(const vector<size_t>& bins,double min,double max,size_t 
             max_count=b;
         }
     }
-    for (size_t bin : bins)
-    {
-        size_t width=50;
-        if(maximum>MAX_ASTERISK)
+
+        if (max_count > MAX_ASTERISK)
         {
-            width=MAX_ASTERISK*((static_cast<double>(bin))/max_count);
+            const double scaling_multiplier = (double)MAX_ASTERISK / max_count;
+            if(max_count*BLOCK_WIDTH/GRAPH_WIDTH>1)
+            {
+                for (size_t bin : bins)
+                {
+                    const double new_scaling_multiplier=(double)GRAPH_WIDTH/(max_count*BLOCK_WIDTH*scaling_multiplier);
+                    const size_t new_bin_width = (size_t)BLOCK_WIDTH*bin*scaling_multiplier*new_scaling_multiplier;
+                    svg_text(TEXT_LEFT+25, top + TEXT_BASELINE, to_string(bin));
+                    svg_rect(TEXT_WIDTH+25, top, new_bin_width, BIN_HEIGHT,"lawngreen","#7CFC00");
+                    top += BIN_HEIGHT;
+                    if(border<max)
+                    {
+                        svg_text(TEXT_LEFT, top + TEXT_BASELINE, to_string(border));
+                        top += BIN_HEIGHT;
+                        border=border+wag;
+                    }
+                }
+            }
+            else
+            {
+                for (size_t bin : bins)
+                {
+                    const double bin_width = BLOCK_WIDTH*bin*scaling_multiplier;
+                    svg_text(TEXT_LEFT+25, top + TEXT_BASELINE, to_string(bin));
+                    svg_rect(TEXT_WIDTH+25, top,bin_width, BIN_HEIGHT,"lawngreen","#7CFC00");
+                    top += BIN_HEIGHT;
+                    if(border<max)
+                    {
+                        svg_text(TEXT_LEFT, top + TEXT_BASELINE, to_string(border));
+                        top += BIN_HEIGHT;
+                        border=border+wag;
+                    }
+                }
+            }
         }
         else
         {
-            width=bin;
+            if(max_count*BLOCK_WIDTH/GRAPH_WIDTH>1)
+            {
+                for (size_t bin : bins)
+                {
+                    const double new_scaling_multiplier=(double)GRAPH_WIDTH/(max_count*BLOCK_WIDTH);
+                    const size_t new_bin_width = (size_t)BLOCK_WIDTH*bin*new_scaling_multiplier;
+                    svg_text(TEXT_LEFT+25, top + TEXT_BASELINE, to_string(bin));
+                    svg_rect(TEXT_WIDTH+25, top,new_bin_width, BIN_HEIGHT,"lawngreen","#7CFC00");
+                    top += BIN_HEIGHT;
+                    if(border<max)
+                    {
+                        svg_text(TEXT_LEFT, top + TEXT_BASELINE, to_string(border));
+                        top += BIN_HEIGHT;
+                        border=border+wag;
+                    }
+                }
+            }
+            else
+            {
+                for (size_t bin : bins)
+                {
+                    const double bin_width = BLOCK_WIDTH * bin;
+                    svg_text(TEXT_LEFT+25, top + TEXT_BASELINE, to_string(bin));
+                    svg_rect(TEXT_WIDTH+25, top,bin_width, BIN_HEIGHT,"lawngreen","#7CFC00");
+                    top += BIN_HEIGHT;
+                    if(border<max)
+                    {
+                        svg_text(TEXT_LEFT, top + TEXT_BASELINE, to_string(border));
+                        top += BIN_HEIGHT;
+                        border=border+wag;
+                    }
+                }
+            }
         }
-        width=BLOCK_WIDTH*width;
-        const double bin_width = width;
-        svg_text(TEXT_LEFT+25, top + TEXT_BASELINE, to_string(bin));
-        svg_rect(TEXT_WIDTH+25, top, bin_width, BIN_HEIGHT,"lawngreen","#7CFC00");
-        top += BIN_HEIGHT;
-        if(border<max)
-        {
-            svg_text(TEXT_LEFT, top + TEXT_BASELINE, to_string(border));
-            top += BIN_HEIGHT;
-            border=border+wag;
-        }
-    }
-    svg_end();
+        svg_end();
 }
 
 
--- lab03-test/test-dz.cpp
+++ lab03-test/test-dz.cpp
@@ -2,7 +2,6 @@
 #include <cassert>
 #include <string.h>
 
-
 void test_1()
 {
 fun_wag(2,4,2);
@@ -15,8 +14,22 @@ fun_wag(2,10,4);
 assert(wag == 2 );
 }
 
+void test_3()
+{
+fun_wag(2,2,2);
+assert(wag==0);
+}
+
+void test_4()
+{
+    fun_wag(2,2,0);
+    assert(wag==0);
+}
+
 int main()
 {
 test_1();
 test_2();
+test_3();
+test_4();
 }
