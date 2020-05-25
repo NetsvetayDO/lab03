3faa335234f09afd91b16f23e14405f0ceac17b5
NetsvetayDO <nedeol@mail.ru>
2020-05-25 19:40:32 +0300

    code: 3.2.6

--- 12/main.cpp
+++ 12/main.cpp
@@ -1,16 +1,14 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include<curl/curl.h>
 #include "histogram.h"
 #include "svg.h"
-#include <curl/curl.h>
 using namespace std;
 
-vector<double> input_numbers(istream& in, const size_t count)
-{
+vector<double> input_numbers(istream& in, const size_t count) {
     vector<double> result(count);
-    for (size_t i = 0; i < count; i++)
-    {
+    for (size_t i = 0; i < count; i++) {
         in >> result[i];
     }
 
@@ -18,8 +16,7 @@ vector<double> input_numbers(istream& in, const size_t count)
 }
 
 Input
-read_input(istream& in, bool prompt)
-{
+read_input(istream& in, bool prompt) {
     Input data;
     size_t number_count;
 
@@ -40,24 +37,26 @@ read_input(istream& in, bool prompt)
         data.numbers = input_numbers(in, number_count);
         in >> data.bin_count;
     }
-
-
     return data;
 }
 
-int main()
+int main(int argc, char* argv[])
 {
+
+    if (argc > 1)
+    {
+        CURL *curl = curl_easy_init();
+        if(curl) {
+            CURLcode res;
+            curl_easy_setopt(curl, CURLOPT_URL, argv[1]);
+            res = curl_easy_perform(curl);
+            curl_easy_cleanup(curl);
+        }
+        return 0;
+    }
     curl_global_init(CURL_GLOBAL_ALL);
-
-
     Input data = read_input(cin, true);
-
-
     const auto bins = make_histogram(data);
-
-
     show_histogram_svg(bins);
-
-
     return 0;
 }
