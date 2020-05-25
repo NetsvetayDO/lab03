50de71c7183c8db462e33d593872baf6bbae53a1
NetsvetayDO <nedeol@mail.ru>
2020-05-25 19:41:53 +0300

    code: 3.2.7

--- 12/main.cpp
+++ 12/main.cpp
@@ -6,9 +6,11 @@
 #include "svg.h"
 using namespace std;
 
-vector<double> input_numbers(istream& in, const size_t count) {
+vector<double> input_numbers(istream& in, const size_t count)
+{
     vector<double> result(count);
-    for (size_t i = 0; i < count; i++) {
+    for (size_t i = 0; i < count; i++)
+    {
         in >> result[i];
     }
 
@@ -16,7 +18,8 @@ vector<double> input_numbers(istream& in, const size_t count) {
 }
 
 Input
-read_input(istream& in, bool prompt) {
+read_input(istream& in, bool prompt)
+{
     Input data;
     size_t number_count;
 
@@ -37,6 +40,8 @@ read_input(istream& in, bool prompt) {
         data.numbers = input_numbers(in, number_count);
         in >> data.bin_count;
     }
+
+
     return data;
 }
 
@@ -46,17 +51,33 @@ int main(int argc, char* argv[])
     if (argc > 1)
     {
         CURL *curl = curl_easy_init();
-        if(curl) {
+        if(curl)
+        {
             CURLcode res;
             curl_easy_setopt(curl, CURLOPT_URL, argv[1]);
             res = curl_easy_perform(curl);
+            res = CURLE_FAILED_INIT;
+            if (res != CURLE_OK)
+            {
+                cout << curl_easy_strerror(res) << endl;
+                exit(1);
+            }
             curl_easy_cleanup(curl);
         }
         return 0;
     }
+
     curl_global_init(CURL_GLOBAL_ALL);
+
+    // Ââîä äàííûõ
     Input data = read_input(cin, true);
+
+    // Îáðàáîòêà äàííûõ
     const auto bins = make_histogram(data);
+
+    // Âûâîä äàííûõ
     show_histogram_svg(bins);
+    //show_histogram_text(bins);
+
     return 0;
 }
