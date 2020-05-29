15263a25070073632bd12ebfa644bd84f5198fda
NetsvetayDO <nedeol@mail.ru>
2020-05-29 12:09:48 +0300

    code: 3.2b

--- 12/main.cpp
+++ 12/main.cpp
@@ -38,8 +38,11 @@ vector <size_t> make_histogram(const vector<double>& numbers,size_t bin_count)
 
 int main()
 {
-     printf("version10 = %lu\n",GetVersion());
-    printf("version16 = %08lx\n",GetVersion());
+      DWORD info = GetVersion();
+    DWORD mask = 0b00000000'00000000'11111111'11111111;
+    DWORD version = info & mask;
+    printf("version10 = %lu\n",version);
+    printf("version16 = %08lx\n",version);
     return 0;
     size_t number_count;
     cerr << "Enter number count: ";
